f8b8bbd6434916dcb648b528fa35782895e242de
John Paul <johnpaultolentino5@gmail.com>
2024-12-12 14:29:05 +0800

    updated clear button

--- app/src/main/java/com/example/gradecalculator/MainActivity2.kt
+++ app/src/main/java/com/example/gradecalculator/MainActivity2.kt
@@ -1,35 +1,49 @@
 package com.example.gradecalculator
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
+
+    @SuppressLint("DefaultLocale")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
 
-        val num1 = findViewById<EditText>(R.id.prelimGrade)
-        val num2 = findViewById<EditText>(R.id.midtermGrade)
-        val num3 = findViewById<EditText>(R.id.finalGrade)
+        val prelim = findViewById<EditText>(R.id.prelimGrade)
+        val midterm = findViewById<EditText>(R.id.midtermGrade)
+        val finals = findViewById<EditText>(R.id.finalGrade)
         val ave = findViewById<TextView>(R.id.gradeAverage)
         val comp = findViewById<Button>(R.id.btnCompute)
+        val clearTxt = findViewById<Button>(R.id.btnClear)
+
         comp.setOnClickListener {
-            val grade1 = num1.text.toString().toInt()
-            val grade2 = num2.text.toString().toInt()
-            val grade3 = num3.text.toString().toInt()
+            val grade1 = prelim.text.toString().toInt()
+            val grade2 = midterm.text.toString().toInt()
+            val grade3 = finals.text.toString().toInt()
 
-            val average = ( (grade1 * 0.30) + (grade2 * 0.30) + (grade3 * 0.40) )
-            val twoDecimal = String.format("%.2f", average)
+            //set only up to 100 grade input
+            if(prelim.text.isEmpty() || midterm.text.isEmpty() || finals.text.isEmpty()){
+                Toast.makeText(this, "Error: Fill prelim, midterm, and final grade to compute", Toast.LENGTH_LONG).show()
+            }
+            if(grade1 > 100 || grade1 < 0 || grade2 > 100 || grade2 < 0 || grade3 > 100 || grade3 < 0){
+                Toast.makeText(this, "Error: Enter number between 0 and 100 only", Toast.LENGTH_LONG).show()
+            }else{
+                val average = ( (grade1 * 0.30) + (grade2 * 0.30) + (grade3 * 0.40) )
+                val twoDecimal = String.format("%.2f", average)
 
-            ave.text = twoDecimal
+                ave.text = twoDecimal
+            }
+
         }
 
         val btnAlert = findViewById<Button>(R.id.btnClose)
@@ -50,6 +64,14 @@ class MainActivity2 : AppCompatActivity() {
 
             val alertDialogBox = alertDialogBuilder.create()
             alertDialogBox.show()
+
+        }
+
+        clearTxt.setOnClickListener{
+
+            prelim.setText("")
+            midterm.setText("")
+            finals.setText("")
 
         }
 
